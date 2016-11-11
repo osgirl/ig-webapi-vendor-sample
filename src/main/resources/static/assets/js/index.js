8a04f82dd634f7ce62275618fb161c9045405b54
Lucas Gut <Lucas.GutGalan@ig.com>
2016-11-11 12:32:52 +0000

    TDRPA-47 [OAuth] Test the implicit grant flow for B2B vendors - Add a link on the result page to request a new access token via the implicit grant flow.

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -2,27 +2,3 @@ window.onload = function() {
     $("#authorization_code_grant_link").attr("href", getAuthorizationCodeGrantUrl());
     $("#implicit_grant_link").attr("href", getImplicitGrantUrl());
 };
-
-function getAuthorizationCodeGrantUrl() {
-    var authRequestParameters = {
-        "response_type": "code",
-        "client_id": client_id,
-        "realm": client_realm,
-        "scope": "profile",
-        "redirect_uri": vendorBaseUrl + "/api-vendor-sample/authorization-handler",
-        "state": state
-    };
-    return oamBaseUrl + "/oauth2/authorize?" + encodeQueryData(authRequestParameters);
-}
-
-function getImplicitGrantUrl() {
-    var authRequestImplicitParameters = {
-        "response_type": "token",
-        "client_id": client_id,
-        "realm": client_realm,
-        "scope": "profile",
-        "redirect_uri": vendorBaseUrl + "/api-vendor-sample/implicit-grant-handler.html",
-        "state": state
-    };
-    return oamBaseUrl + "/oauth2/authorize?" + encodeQueryData(authRequestImplicitParameters);
-}
--- src/main/resources/static/assets/js/oauth/common.js
+++ src/main/resources/static/assets/js/oauth/common.js
@@ -32,3 +32,27 @@ function parseQueryString() {
     }
     return query;
 }
+
+function getAuthorizationCodeGrantUrl() {
+    var authRequestParameters = {
+        "response_type": "code",
+        "client_id": client_id,
+        "realm": client_realm,
+        "scope": "profile",
+        "redirect_uri": vendorBaseUrl + "/api-vendor-sample/authorization-handler",
+        "state": state
+    };
+    return oamBaseUrl + "/oauth2/authorize?" + encodeQueryData(authRequestParameters);
+}
+
+function getImplicitGrantUrl() {
+    var authRequestImplicitParameters = {
+        "response_type": "token",
+        "client_id": client_id,
+        "realm": client_realm,
+        "scope": "profile",
+        "redirect_uri": vendorBaseUrl + "/api-vendor-sample/implicit-grant-handler.html",
+        "state": state
+    };
+    return oamBaseUrl + "/oauth2/authorize?" + encodeQueryData(authRequestImplicitParameters);
+}
